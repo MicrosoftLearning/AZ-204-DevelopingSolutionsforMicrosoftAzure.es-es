2bdb246b49ef0e404dfb0367d80a0a5d74f409c4
olprod <olprod@microsoft.com>
2021-12-08 12:07:03 +0000

    Copy Files From Source Repo (2021-12-08 12:07)

---
+++ Allfiles/Labs/09/Solution/EventPublisher/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Messaging.EventGrid;
+
+public class Program
+{
+    private const string topicEndpoint = "[PLACE YOUR TOPIC ENDPOINT HERE]";
+
+    private const string topicKey = "[PLACE YOUR TOPIC KEY HERE]";
+
+    public static async Task Main(string[] args)
+    {
+        Uri endpoint = new Uri(topicEndpoint);
+        AzureKeyCredential credential = new AzureKeyCredential(topicKey);
+        EventGridPublisherClient client = new EventGridPublisherClient(endpoint, credential);
+        
+        EventGridEvent firstEvent = new EventGridEvent(
+            subject: $"New Employee: Alba Sutton",
+            eventType: "Employees.Registration.New",
+            dataVersion: "1.0",
+            data: new
+            {
+                FullName = "Alba Sutton",
+                Address = "4567 Pine Avenue, Edison, WA 97202"
+            }
+        );
+
+        EventGridEvent secondEvent = new EventGridEvent(
+            subject: $"New Employee: Alexandre Doyon",
+            eventType: "Employees.Registration.New",
+            dataVersion: "1.0",
+            data: new
+            {
+                FullName = "Alexandre Doyon",
+                Address = "456 College Street, Bow, WA 98107"
+            }
+        );
+
+        await client.SendEventAsync(firstEvent);
+        Console.WriteLine("First event published");
+
+        await client.SendEventAsync(secondEvent);
+        Console.WriteLine("Second event published");
+    }
+}
